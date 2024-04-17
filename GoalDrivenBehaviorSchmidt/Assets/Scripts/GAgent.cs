cbaaef19fa3ba775744560f0cc4242fcd0e0a0a4
Ashley Schmidt <jcvxka@gmail.com>
2024-04-16 22:56:49 -0700

    added script
    finished executing a simple plan. starting create a multi-step plan

--- GoalDrivenBehaviorSchmidt/Assets/Scripts/GAgent.cs
+++ GoalDrivenBehaviorSchmidt/Assets/Scripts/GAgent.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using System.Linq;
 using UnityEditor.Search;
+using Unity.VisualScripting;
 
 public class SubGoal
 {
@@ -26,16 +27,85 @@ public class GAgent : MonoBehaviour
     SubGoal currentGoal;
 
     // Start is called before the first frame update
-    void Start()
+    protected virtual void Start()
     {
         GAction[] acts = this.GetComponents<GAction>();
         foreach (GAction a in acts)
             actions.Add(a);
     }
 
+    bool invoked = false;
+    void CompleteAction()
+    {
+        currentAction.running = false;
+        currentAction.PostPerform();
+        invoked = false;
+    }
+
     // Update is called once per frame
     void LateUpdate()
     {
-        
+        if(currentAction != null && currentAction.running)
+        {
+            if(currentAction.agent.hasPath && currentAction.agent.remainingDistance < 1f)
+            {
+                if(!invoked)
+                {
+                    Invoke("CompleteAction", currentAction.duration);
+                    invoked = true;
+                }
+            }
+            return;
+        }
+
+        if(planner == null || actionQueue == null)
+        {
+            planner = new GPlanner();
+
+            var sortedGoals = from entry in goals orderby entry.Value descending select entry;
+            foreach(KeyValuePair<SubGoal, int> sg in sortedGoals)
+            {
+                actionQueue = planner.plan(actions, sg.Key.sgoals, null);
+                if(actionQueue != null)
+                {
+                    currentGoal = sg.Key;
+                    break;
+                }
+            }
+
+        }
+
+        if(actionQueue != null && actionQueue.Count == 0)
+        {
+            if(currentGoal.remove)
+            {
+                goals.Remove(currentGoal);
+
+            }
+            planner = null;
+        }
+
+        if(actionQueue != null && actionQueue.Count > 0)
+        {
+            currentAction = actionQueue.Dequeue();
+            if(currentAction.PostPerform())
+            {
+                if (currentAction.target == null && currentAction.targetTag != "")
+                    currentAction.target = GameObject.FindWithTag(currentAction.targetTag);
+
+                if(currentAction.target != null)
+                {
+                    currentAction.running = true;
+                    currentAction.agent.SetDestination(currentAction.target.transform.position);
+                }
+
+            }
+            else
+            {
+                actionQueue = null;
+
+            }
+        }
+
     }
 }
