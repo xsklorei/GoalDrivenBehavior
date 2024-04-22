dce60cc8c490e325d12da332720f6b3e03c0c19e
Ashley Schmidt <jcvxka@gmail.com>
2024-04-21 23:03:36 -0700

    multi-step plan
    it does not go to waiting room but stays on go to hospital for no valid reason

--- GoalDrivenBehaviorSchmidt/Assets/Scripts/GOAP/GAgent.cs
+++ GoalDrivenBehaviorSchmidt/Assets/Scripts/GOAP/GAgent.cs
@@ -47,8 +47,10 @@ public class GAgent : MonoBehaviour
     {
         if(currentAction != null && currentAction.running)
         {
-            if(currentAction.agent.hasPath && currentAction.agent.remainingDistance < 1f)
+            float distanceToTarget = Vector3.Distance(currentAction.target.transform.position, this.transform.position);
+            if(currentAction.agent.hasPath && distanceToTarget < 2f)//currentAction.agent.remainingDistance < 1f)
             {
+                Debug.Log("Distance to Goal: " + currentAction.agent.remainingDistance);
                 if(!invoked)
                 {
                     Invoke("CompleteAction", currentAction.duration);
